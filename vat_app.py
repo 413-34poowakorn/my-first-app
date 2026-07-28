feb50ffe64698d0a1d7e3d1b50d182cc312adcbd
413-34poowakorn <56876@yupparaj.ac.th>
2026-07-28 21:06:59 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -3,7 +3,7 @@ st.title("แอปพลิเคชันคำนวณราคาสิน�
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * p0.07
 net_price = price - vet
-st.header(f” ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท”)
+st.header(f"ภาษีมูลค่าเพิ่ม (VAT 7%): {vat:.2f} บาท")
 st.header(f"•ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
 st.write(“นาย ภูวกร ปรีชาะนัทสกุลเลขที่33 ม4/13”)
